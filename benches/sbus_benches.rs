0aa03fb2aa7fc458e710318b623a258c531584d4
Ragarnoy <tiagolernould@gmail.com>
2024-11-07 12:13:34 +0100

    Update benches/sbus_benches.rs

--- benches/sbus_benches.rs
+++ benches/sbus_benches.rs
@@ -148,7 +148,7 @@ fn bench_async_parser(c: &mut Criterion) {
     group.finish();
 }
 
-// Include the pack_channels function from your tests
+Pack channels function from tests
 fn pack_channels(buffer: &mut [u8; SBUS_FRAME_LENGTH], channels: &[u16; 16]) {
     buffer
         .iter_mut()

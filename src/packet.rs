5f2c29b64678103835f4016bf496e3c21c0fcecd
Omelia Iliffe <omelia.iliffe@wetaworkshop.co.nz>
2025-04-03 15:35:45 +1300

    add serde feature

--- src/packet.rs
+++ src/packet.rs
@@ -3,6 +3,7 @@ use crate::{channels_parsing, SbusError, SBUS_FOOTER, SBUS_FRAME_LENGTH, SBUS_HE
 /// Represents a complete SBUS packet with channel data and flags
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 #[cfg_attr(feature = "defmt-03", derive(defmt::Format))]
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
 pub struct SbusPacket {
     pub channels: [u16; 16],
     pub flags: Flags,
@@ -47,6 +48,7 @@ impl SbusPacket {
 /// Status flags contained in an SBUS frame
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 #[cfg_attr(feature = "defmt-03", derive(defmt::Format))]
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
 pub struct Flags {
     pub d1: bool,
     pub d2: bool,
